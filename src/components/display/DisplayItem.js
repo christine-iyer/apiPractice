4e1e389afd0eca065b7ed489e0da8f4c3ec4479a
chrisIyer <hellohotbox420@gmail.com>
2023-02-15 20:35:00 -0500

    layout works.

--- src/components/display/DisplayItem.js
+++ src/components/display/DisplayItem.js
@@ -4,23 +4,23 @@ import Card from 'react-bootstrap/Card'
 
 const DisplayItem = ({ result }) => {
      return (
-          <Card 
-          className="cards"
-          // style={{ 
-          //      width: '15rem', 
-          //      backgroundColor: "blue", 
-          //      display: "grid",
-          //      alignText: "left", 
-          //      gap: "1rem"}}
-                >
-               <Card.Body>
-                    <Card.Title>{result.symbol}</Card.Title>
-                    <Card.Subtitle>{result.fullExchangeName}</Card.Subtitle>
-                    <Card.Text>{result.regularMarketDayHigh}</Card.Text>
+          
+               <Card
 
-
-               </Card.Body>
-          </Card>
+                    style={{
+                         width: '15rem',
+                         backgroundColor: "blue",
+                         display: "grid",
+                         alignText: "left",
+                         gap: "1rem"
+                    }}>
+                    <Card.Body>
+                         <Card.Title>{result.symbol}</Card.Title>
+                         <Card.Subtitle>{result.fullExchangeName}</Card.Subtitle>
+                         <Card.Text>{result.regularMarketDayHigh}</Card.Text>
+                    </Card.Body>
+               </Card>
+          
      )
 }
 export default DisplayItem;
