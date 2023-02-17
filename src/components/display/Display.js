fc89f5917929e6547ce96fd690420af2d080103b
chrisIyer <hellohotbox420@gmail.com>
2023-02-16 19:19:18 -0500

    not moving not labeling.

--- src/components/display/Display.js
+++ src/components/display/Display.js
@@ -1,26 +1,24 @@
 import React from 'react'
 import DisplayItem from './DisplayItem'
-import Container  from 'react-bootstrap/Container'
-import { DndContext,
-closestCenter } from '@dnd-kit/core'
+import Container from 'react-bootstrap/Container'
+import {
+  DndContext,
+  closestCenter
+} from '@dnd-kit/core'
 import { arrayMove, SortableContext, verticalListSortingStrategy } from '@dnd-kit/sortable';
 
 const Display = ({ results, errorMessage }) => {
   return (
-     !results ? errorMessage : 
-     (<section className='section'>
+    !results ? errorMessage :
+      (<section className='section'>
 
-          {results.map((result) =>(
-          <DisplayItem 
-          key={result.symbol} 
-          result={result} />
+        {results.map((result) => (
+          <DisplayItem
+            key={result.symbol}
+            result={result} />
+        ))}
+      </section>)
 
-
-          ))}
-
-          
-     </section>)
-  
   )
 }
 
