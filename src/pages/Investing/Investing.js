1ac662c8792f6e4f7a1e0c8d01f90735b467356e
chrisIyer <hellohotbox420@gmail.com>
2023-02-17 19:54:42 -0500

    added reading hard coded array.

--- src/pages/Investing/Investing.js
+++ src/pages/Investing/Investing.js
@@ -1,67 +1,23 @@
-// import React from 'react'
-// import { useState, useEffect } from 'react'
+import { assets } from './components/data';
 
-// export default function Investing(){
-   
-// }
-
-
-
-import logo from './logo.svg';
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Container from 'react-bootstrap/Container';
-import {
-  DndContext,
-  closestCenter
-} from "@dnd-kit/core";
-import {
-  arrayMove,
-  SortableContext,
-  verticalListSortingStrategy
-} from "@dnd-kit/sortable";
-import {useState} from 'react';
-import { SortableItem } from './SortableItem';
-
-function App() {
-  const [languages, setLanguages ] = useState(["JavaScript", "Python", "TypeScript"]);
-
-  return (
-    <DndContext
-      collisionDetection={closestCenter}
-      onDragEnd={handleDragEnd}
-    >
-      <Container className="p-3" style={{"width": "50%"}} align="center">
-        <h3>The best programming languages!</h3>
-        <SortableContext
-          items={languages}
-          strategy={verticalListSortingStrategy}
-        >
-          {/* We need components that use the useSortable hook */}
-          {languages.map(language => <SortableItem key={language} id={language}/>)}
-        </SortableContext>
-      </Container>
-    </DndContext>
-  );
-
-  function handleDragEnd(event) {
-    console.log("Drag end called");
-    const {active, over} = event;
-    console.log("ACTIVE: " + active.id);
-    console.log("OVER :" + over.id);
-
-    if(active.id !== over.id) {
-      setLanguages((items) => {
-        const activeIndex = items.indexOf(active.id);
-        const overIndex = items.indexOf(over.id);
-        console.log(arrayMove(items, activeIndex, overIndex));
-        return arrayMove(items, activeIndex, overIndex);
-        // items: [2, 3, 1]   0  -> 2
-        // [1, 2, 3] oldIndex: 0 newIndex: 2  -> [2, 3, 1] 
-      });
-      
-    }
-  }
+export default function List() {
+	const listItems = assets.map((asset) => (
+		<li key={asset.symbol}>
+			<p>
+				<b>{asset.symbol}:</b>
+				{' ' +
+					asset.shares +
+					' shares @ $' +
+					asset.purchasePrice +
+					' cost $' +
+					asset.purchasePrice * asset.shares.toFixed(2)}
+			</p>
+		</li>
+	));
+	return (
+		<article>
+			<h1>Scientists</h1>
+			<ul>{listItems}</ul>
+		</article>
+	);
 }
-
-export default App;
