34646d54fd09d5890cffa38c2bd462f1391e1953
chrisIyer <hellohotbox420@gmail.com>
2023-02-22 13:55:18 -0500

    Haven't tested, just studying files and making little changes.

---
+++ models/assetSchema.js
@@ -0,0 +1,10 @@
+//const { Schema, model } = require('mongoose')
+
+const assetSchema = new Schema({
+   symbol: { type: String, required: true },
+   purchasePrice: { type: Number, default: 0 },
+   shares: { type: Number, default: 0 },
+   regularMarketDayHigh: { type: Number, default: 0 },
+   fullExchangeName: { type: String }
+})
+module.exports = assetSchema
